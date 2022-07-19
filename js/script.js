475cbdb7785df3cc0eacf576aa06d6713a280173
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-07-19 02:18:09 -0300

    Animações de remoção e adição de tarefas

--- js/script.js
+++ js/script.js
@@ -3,46 +3,62 @@ const btnAddTarefa = document.querySelector('#btnAddTarefa');
 const inputTarefas = document.querySelector('#inputTarefas');
 const jsLista = document.querySelector('.jsLista');
 
-function addTarefa() {
 
-  let tarefaItem = document.createElement('li');
-  jsLista.appendChild(tarefaItem);
-
-  let tarefaNome = document.createElement('p');
-  tarefaItem.appendChild(tarefaNome);
-
-  let button = document.createElement('button');
-  tarefaItem.appendChild(button);
-  
-  
-  tarefaNome.innerHTML = inputTarefas.value;
-
-  inputTarefas.value = '';
-}
 
 inputTarefas.addEventListener('keypress', (event) => {
   if(event.keyCode === 13) {
     addTarefa();
+    removeTarefa();
   };
 });
 
 btnAddTarefa.addEventListener('click', () => {
   addTarefa();
-  teste();
+  removeTarefa();
 });
 
+function addTarefa() {
+  let tarefaItem = document.createElement('li');
+  jsLista.appendChild(tarefaItem);
+
+  let tarefaNome = document.createElement('p');
+  tarefaItem.appendChild(tarefaNome);
+  tarefaNome.innerHTML = inputTarefas.value;
+
+  let button = document.createElement('button');
+  tarefaItem.appendChild(button);
+  
+  inputTarefas.value = '';
+};
 
 
 // Remove Tarefa
-
+function removeTarefa() {
   const removeTarefa = jsLista.querySelectorAll('li button');
   const itemTarefa = jsLista.querySelectorAll('li');
   
-  removeTarefa.forEach((btn) => {
+  removeTarefa.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-      console.log('sidh')
+      itemTarefa[index].classList.add('ativo');
+      setTimeout(() => {
+        itemTarefa[index].remove();
+      }, 300);
     });
   });
+};
+removeTarefa();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
